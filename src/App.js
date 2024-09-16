9d6dc2bff414d6b7d0cfa2f95a330c23c3ac9234
SRINIVASANCN <157290991+SRINIVASANCN@users.noreply.github.com>
2024-09-16 18:46:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Todo from './Components/Todo'
+
+const App = () => {
+  return (
+    <div className='bg-stone-900 grid py-4  min-h-screen '>
+       <Todo />
+    </div>
+  )
+}
+
+export default App
+
